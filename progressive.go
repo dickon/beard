3ff9bca95869ec5f24e9767f5dc187bcbe7cd7e7
Dickon Reed <dickon@cantab.net>
2015-04-06 12:29:27 +0100

    green bar: populate a blocks map

--- progressive.go
+++ progressive.go
@@ -7,10 +7,12 @@ type Scanner struct {
         scanned uint
 	hashes [] uint32
 	rolling hash.Hash32
+	blocks map[uint32] [][]byte
 }
 
 func New(window uint) Scanner {
-	return Scanner{window,0,make([]uint32, 0, 1), rollsum.New(uint32(window))}
+	return Scanner{window,0,make([]uint32, 0, 1), rollsum.New(uint32(window)),
+		make(map[uint32] [][]byte)}
 }
 
 func (p *Scanner) Scan(data []byte) {
@@ -18,7 +20,13 @@ func (p *Scanner) Scan(data []byte) {
 		p.rolling.Write(data[i:i+1])
 		p.scanned ++
 		if (p.scanned >= p.window)  {
-			p.hashes = append(p.hashes, p.rolling.Sum32())
+			csum := p.rolling.Sum32()
+			p.hashes = append(p.hashes, csum)
+			start := i+1 - int(p.window)
+			if start >= 0 {
+				p.blocks[csum] = append(p.blocks[csum], data[start:i+1])
+			}
+			
 		}
 	}
 }
